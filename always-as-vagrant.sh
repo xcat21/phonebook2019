cf1b2b91a5c32ab0e1ea58e3465ff817845d82d4
xcat21 <xcat@bk.ru>
2019-03-12 13:34:25 +0300

    Add files via upload
    check one

---
+++ always-as-vagrant.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#== Bash helpers ==
+
+function info {
+  echo " "
+  echo "--> $1"
+  echo " "
+}
+
+#== Provision script ==
+
+info "Provision-script user: `whoami`"
+
+export LANG=C
+
+cd /app
+phalcon migration run
